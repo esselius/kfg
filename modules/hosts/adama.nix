1e2b6187bf4607db60149a4ea06efb89e8608622
Peter Esselius <pepp@me.com>
2025-11-17 23:28:27 +0100

    gitignore result symlink + adama fileSystems

--- modules/hosts/adama.nix
+++ modules/hosts/adama.nix
@@ -1,5 +1,18 @@
 {
   den.aspects.adama = {
-    nixos.system.stateVersion = "24.05";
+    nixos = {
+      system.stateVersion = "24.05";
+      fileSystems = {
+        "/" = {
+          device = "/dev/disk/by-label/NIXOS_SD";
+          fsType = "ext4";
+        };
+        "/boot/firmware" = {
+          device = "/dev/disk/by-label/FIRMWARE";
+          fsType = "vfat";
+        };
+      };
+    };
+
   };
 }
